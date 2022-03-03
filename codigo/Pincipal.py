bc8e52d51b8de8eee7bb1c28ddfc548f6b3ead22
MateoRamirezRubio1 <100296963+MateoRamirezRubio1@users.noreply.github.com>
2022-03-02 22:01:42 -0500

    Update Pincipal.py

--- codigo/Pincipal.py
+++ codigo/Pincipal.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv("calles_de_medellin_con_acoso.csv",sep=';')
-print(df)
+print("Pandas calles de Medellín:\n")
+df_calles = pd.read_csv("calles_de_medellin_con_acoso.csv",sep=';')
+print(df_calles)
+
+print("\n\nPandas polígono de Medellín:\n")
+df_poligono = pd.read_csv("poligono_de_medellin.csv",sep=';')
+print(df_poligono)
