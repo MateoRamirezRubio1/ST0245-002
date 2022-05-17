85243739f95873ca4d5d9f140d0338e87d376c41
MateoRamirezRubio1 <100296963+MateoRamirezRubio1@users.noreply.github.com>
2022-05-16 20:55:43 -0500

    Update Pincipal.py

--- codigo/Pincipal.py
+++ codigo/Pincipal.py
@@ -1,151 +1,126 @@
+import networkx as nx
 import pandas as pd
-from collections import defaultdict
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from shapely import wkt
 
-class Vertice:
-    def __init__(self, i):
-        self.id = i
-        self.vecinos = []
-        self.transitado = False
-        self.padre = None
-        self.distancia = float('inf')
+#Cargar datos y creación de diccionarios
+df_calles = pd.read_csv("calles_de_medellin_con_acoso.csv",sep=';')
+df_calles['geometry'] = df_calles['geometry'].apply(wkt.loads)
+df_calles = gpd.GeoDataFrame(df_calles)
 
-    def agregarVecino(self, v, p):
-        if v not in self.vecinos:
-            self.vecinos.append([v, p])
+area = pd.read_csv('poligono_de_medellin.csv',sep=';')
+area['geometry'] = area['geometry'].apply(wkt.loads)
+area = gpd.GeoDataFrame(area)
 
-class Grafo:
-    def __init__(self):
-        self.vertices = {}
+df_names = df_calles.drop(['destination', 'length', 'oneway', 'harassmentRisk', 'geometry'], axis=1)
 
-    def agregarVertice(self, id):
-        if id not in self.vertices:
-            self.vertices[id] = Vertice(id)
+table_names = dict()
+table_datos_grafoNX_camino_corto = dict()
+table_datos_camino_menor_acoso = dict()
+lista_camino_corto = []
+lista_camino_menor_acoso = []
 
-    def agregarArista(self, a , b, p): # a = origen, b = destino, p = peso
-        if a in self.vertices and b in self.vertices:
-            self.vertices[a].agregarVecino(b, p)
-            self.vertices[b].agregarVecino(a, p)
+# Hallar el promedio de acoso para rellenar campos vacios
+promedio_acoso = 0
+contador = 0
+for i in range(len(df_calles)):
+    if not pd.isna(df_calles['harassmentRisk'][i]):
+        promedio_acoso += df_calles['harassmentRisk'][i]
+        contador += 1
+promedio_acoso /= contador
 
-    def ruta(self, a, b):
-        ruta = []
-        actual = b
-        while actual != None:
-            ruta.insert(0, actual)
-            actual = self.vertices[actual].padre
-        return [ruta, self.vertices[b].distancia]
+# Rellenar campos vacios de los dataframes
+for i in range(len(df_calles['origin'])):
+    #Los niveles de acoso vacíos con el promedio de acoso ponderado
+    if pd.isna((df_calles['harassmentRisk'][i])):
+        df_calles.at[i, 'harassmentRisk'] = promedio_acoso
+    #Las calles que no tienen dato de su distancia se pone con 0
+    if pd.isna((df_calles['length'][i])):
+        df_calles.at[i, 'length'] = 0
+    #Los nombres vacíos se pone el número de indice
+    if pd.isna((df_names['name'][i])):
+        df_names.at[i, 'name'] = str(i)
+    #Se ponen los datos del nombre con su respectivo origen en el diccionario para ser mostrados al usuario
+    table_names[df_names['name'][i]] = df_names['origin'][i]
 
-    def minimo(self, lista):
-        if len(lista) > 0:
-            m = self.vertices[lista[0]].distancia
-            v = lista[0]
-            for e in lista:
-                if m > self.vertices[e].distancia:
-                    m = self.vertices[e].distancia
-                    v = e
+#Mostrar datos al usuario y obtener el orgien y el destino al que se dirige
+print("Calles Medellín:")
+print("__________________________________")
+print(df_names)
+print("__________________________________")
+print("A donde te quieres dirigir?: (Escriba el nombre de la calle tal cual está en la tabla)")
+user_option_origin = str(input("Origen: "))
+user_option_destination = str(input("Destino: "))
 
-            return v
+if (user_option_origin in table_names) and (user_option_destination in table_names):
+    # Crear el grafo del camino más corto
+    MAP_PATH = nx.from_pandas_edgelist(df_calles, source='origin', target='destination', edge_attr='length')
+    djk_path = nx.dijkstra_path(MAP_PATH, source=table_names[user_option_origin],target=table_names[user_option_destination], weight='length')
 
-    def dijkstra(self, a):
-        if a in self.vertices:
-            self.vertices[a].distancia = 0
-            actual = a
-            noTrancitados = []
+    # Crear el grafo del camino con menor acoso
+    MAP_RISK = nx.from_pandas_edgelist(df_calles, source='origin', target='destination', edge_attr='harassmentRisk')
+    djk_path_harassment = nx.dijkstra_path(MAP_RISK, source=table_names[user_option_origin],target=table_names[user_option_destination], weight='harassmentRisk')
 
-            for v in self.vertices:
-                if v != a:
-                    self.vertices[v].distancia = float('inf')
-                self.vertices[v].padre = None
-                noTrancitados.append(v)
+    # Print the previously created graphs on the screen
+    print("\nEsta es la ruta más corta por seguir:")
+    print(djk_path)
+    print("\nEsta es la ruta con menor acoso por seguir:")
+    print(djk_path_harassment)
 
-            while len(noTrancitados) > 0:
-                for vecino in self.vertices[actual].vecinos:
-                    if self.vertices[vecino[0]].transitado == False:
-                        if self.vertices[actual].distancia + vecino[1] < self.vertices[vecino[0]].distancia:
-                            self.vertices[vecino[0]].distancia = self.vertices[actual].distancia + vecino[1]
-                            self.vertices[vecino[0]].padre = actual
+    # Llenar los diccionarios con los origenes obtenidos
+    # Origenes camino más corto
+    for i in djk_path:
+        table_datos_grafoNX_camino_corto[i] = 0
+    # Origenes camino con menor acoso
+    for i in djk_path_harassment:
+        table_datos_camino_menor_acoso[i] = 0
 
-                self.vertices[actual].transitado = True
-                noTrancitados.remove(actual)
-
-                actual = self.minimo(noTrancitados)
-        else:
-            return False
-
-class main:
-    # Cargar datos y crear diccionarios
-    df_calles = pd.read_csv("calles_de_medellin_con_acoso.csv", sep=';')
-    df_names = df_calles.drop(['destination', 'length', 'oneway', 'harassmentRisk', 'geometry'], axis=1)
-    df_calles = df_calles.drop(['name', 'oneway', 'geometry'], axis=1)
-    table = defaultdict(lambda: "No se presenta")  # Diccionario con los datos de cada calle
-    table_names = defaultdict(lambda: "No se encuentra esta calle")  # Diccionario para obtener el dato ingresado por el usuario
-
-    # Crear tabla para los nombres de las calles
-    for i in range(len(df_names['name'])):
-        if pd.isna((df_names['name'][i])):
-            df_names.at[i, 'name'] = str(i)
-        table_names[df_names['name'][i]] = df_names['origin'][i]
-
-    # Hallar el promedio de acoso para rellenar campos vacios
-    promedio_acoso = 0
-    contador = 0
+    # Obtener las coordenadas de los origenes anteriormente obtenidos
     for i in range(len(df_calles)):
-        if not pd.isna(df_calles['harassmentRisk'][i]):
-            promedio_acoso += df_calles['harassmentRisk'][i]
-            contador += 1
-    promedio_acoso /= contador
+        if df_calles['origin'][i] in table_datos_grafoNX_camino_corto:
+            lista_camino_corto.append(str(df_calles['geometry'][i]))
+        if df_calles['origin'][i] in table_datos_camino_menor_acoso:
+            lista_camino_menor_acoso.append(str(df_calles['geometry'][i]))
 
-    # Rellenar campos vacios de los dataframes
-    for i in range(len(df_calles['origin'])):
-        if pd.isna((df_calles['harassmentRisk'][i])):
-            df_calles.at[i, 'harassmentRisk'] = promedio_acoso
-        if pd.isna((df_calles['length'][i])):
-            df_calles.at[i, 'length'] = 0
+    # Crear dataframes pandas con las coordenadas anteriormente obtenidas para graficar los caminos
+    data_camino_corto = pd.DataFrame(lista_camino_corto, columns=['geometry'])
+    data_camino_corto['geometry'] = data_camino_corto['geometry'].apply(wkt.loads)
+    data_camino_corto = gpd.GeoDataFrame(data_camino_corto)
 
-    # Introducir datos al diccionario correspondiente (creación lista de adyacencia)
-    for i in range(len(df_calles["origin"])):
-        if df_calles["origin"][i] in table:
-            table[str(df_calles["origin"][i])] += [
-                (df_calles["destination"][i], df_calles["length"][i], df_calles["harassmentRisk"][i])]
-        else:
-            table[str(df_calles["origin"][i])] = [(df_calles["destination"][i], df_calles["length"][i], df_calles["harassmentRisk"][i])]
+    data_camino_menor_acoso = pd.DataFrame(lista_camino_menor_acoso, columns=['geometry'])
+    data_camino_menor_acoso['geometry'] = data_camino_menor_acoso['geometry'].apply(wkt.loads)
+    data_camino_menor_acoso = gpd.GeoDataFrame(data_camino_menor_acoso)
 
-    # Pedir datos al usuario
-    print("Calles Medellín:")
-    print("__________________________________")
-    print(df_names)
-    print("__________________________________")
-    print("A donde te quieres dirigir?: (Escriba el nombre de la calle tal cual está en la tabla)")
-    user_option_origin = str(input("Origen: "))
-    user_option_destination = str(input("Destino: "))
+    # Graficar mapa Medellín con el camino más corto
+    # Crear gráfico
+    fig, ax = plt.subplots(figsize=(20, 12))
+    # Graficar fondo
+    area.plot(ax=ax, facecolor='black')
+    # Graficar calles Medellín
+    df_calles.plot(ax=ax, linewidth=1, edgecolor='dimgray')
+    # Graficar camino más corto
+    data_camino_corto.plot(ax=ax, linewidth=1, edgecolor='darkorange')
+    # Mostrar mapa con camino mas corto
+    plt.title('Camino Más Corto')
+    plt.tight_layout()
+    plt.show()
 
-    # Crear grafos
-    grafo_camio_corto = Grafo()
-    grafo_menor_acoso = Grafo()
-    boolean = False
-    for i in table:
-        grafo_camio_corto.agregarVertice(i)
-        grafo_menor_acoso.agregarVertice(i)
-        a = len(table[i])
-        while a > 0:
-            grafo_camio_corto.agregarArista(i, table[i][a - 1][0], table[i][a - 1][1])
-            grafo_menor_acoso.agregarArista(i, table[i][a - 1][0], table[i][a - 1][2])
-            a -= 1
-
-    # Ejecutar algoritmo
-    grafo_camio_corto.dijkstra(table_names[user_option_origin])
-    grafo_menor_acoso.dijkstra(table_names[user_option_origin])
-    grafo_CO = grafo_camio_corto.ruta(table_names[user_option_origin], table_names[user_option_destination])
-    grafo_MA = grafo_menor_acoso.ruta(table_names[user_option_origin], table_names[user_option_destination])
-
-    # Imprimir algoritmo
-    if grafo_MA[1] == float('inf') or grafo_CO[1] == float('inf'):
-        print("La ruta seleccionada no es transitable")
-    else:
-        print("Ruta con menor acoso:")
-        print(grafo_MA[0])
-        print("Acoso total:", end=" ")
-        print(grafo_MA[1])
-        print("\nRuta más corta:")
-        print(grafo_CO[0])
-        print("Distancia total:", end=" ")
-        print(grafo_CO[1])
+    # Graficar mapa Medellín con el camino con menor acoso
+    # Crear gráfico
+    fig, ax = plt.subplots(figsize=(20, 12))
+    # Graficar fondo
+    area.plot(ax=ax, facecolor='black')
+    # Graficar calles Medellín
+    df_calles.plot(ax=ax, linewidth=1, edgecolor='dimgray')
+    # Graficar camino con menor acoso
+    data_camino_menor_acoso.plot(ax=ax, linewidth=1, edgecolor='darkorange')
+    # Mostrar mapa camino con menor acoso
+    plt.title('Camino Con Menor Acoso')
+    plt.tight_layout()
+    plt.show()
+else:
+    if not (user_option_origin in table_names):
+        print("El origen ingresado no se encuentra en la tabla. Vuelva a intentarlo.")
+    if not (user_option_destination in table_names):
+        print("El destino ingresado no se encuentra en la tabla. Vuelva a intentarlo.")
